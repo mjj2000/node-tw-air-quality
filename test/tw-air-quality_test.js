1fd83cc5f36e55cd54ab1a6238d2a600bfc2bc0a
MJJ <mjj2000@gmail.com>
2018-03-03 14:36:45 +0800

    fix test

--- test/tw-air-quality_test.js
+++ test/tw-air-quality_test.js
@@ -5,9 +5,19 @@ module.exports = {
   setUp: function (done) {
     done();
   },
-  'no args': function(test) {
-    test.expect(1);
-    test.equal(twAirQuality.awesome(), 'awesome', 'should be awesome');
+  testMethodsExist: function(test) {
+    test.ok(twAirQuality.queryStatusByCity);
+    test.ok(twAirQuality.queryPm25ByCity);
+    test.ok(twAirQuality.queryPm10ByCity);
+    test.ok(twAirQuality.querySo2ByCity);
+    test.ok(twAirQuality.queryCoByCity);
+    test.ok(twAirQuality.queryO3ByCity);
+    test.ok(twAirQuality.queryNo2ByCity);
+    test.ok(twAirQuality.queryWindSpeedByCity);
+    test.ok(twAirQuality.queryWindDirecByCity);
+    test.ok(twAirQuality.queryFpmiByCity);
+    test.ok(twAirQuality.queryNoxByCity);
+    test.ok(twAirQuality.queryNoByCity);
     test.done();
   }
 };
